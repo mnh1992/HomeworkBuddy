ce0ca0b17bcd8944419017f8609935144ab2a17f
mnh1992 <maheshi.hemachandra@gmail.com>
2016-09-20 06:41:56 -0400

    Files added - jmain.js / login.html / signup.html

---
+++ jmain.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#btn1').click(function(){
+	window.alert("You successfully created an account on WhiteBoard!!!");
+	});
+});
+
+$(document).ready(function(){
+    $('#btn2').click(function(){
+	window.alert("Successfully entered your account");
+	});
+});
