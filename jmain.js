bb8d6c6d1049777f9e6a290c1d6eceddd0d4792b
mnh1992 <maheshi.hemachandra@gmail.com>
2016-09-20 06:58:52 -0400

    Update jmain.js

--- jmain.js
+++ jmain.js
@@ -1,9 +1,11 @@
+//Greet the user when filled out the login page info and hit enter button
 $(document).ready(function(){
     $('#btn1').click(function(){
 	window.alert("You successfully created an account on WhiteBoard!!!");
 	});
 });
 
+//Greet the user for creating an account at WhiteBoard
 $(document).ready(function(){
     $('#btn2').click(function(){
 	window.alert("Successfully entered your account");
